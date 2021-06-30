272d013685bdebc57b7b7072f918776f0d353702
janhenrikfock <jan.henrik.fock@googlemail.com>
2021-06-30 13:34:05 +0200

    Add sidebar to stories

--- src/Components/Sidebar/Sidebar.stories.js
+++ src/Components/Sidebar/Sidebar.stories.js
@@ -5,12 +5,89 @@ import Sidebar from './Sidebar'
 export default {
   title: 'Sidebar',
   component: Sidebar,
+  decorators: [
+    (Sidebar) => (
+      <div
+        style={{
+          width: '40vh',
+        }}
+      >
+        <Sidebar />
+      </div>
+    ),
+  ],
 }
 const Template = (args) => <Sidebar {...args} />
 
 export const Primary = Template.bind({})
 Primary.args = {
-  sortedWorkers: [[], [], []],
-  setopenCompany: {},
-  setRenderNewWorker: false,
+  sortedWorkers: {
+    FirmaGmbH: [
+      {
+        name: 'name',
+        surname: 'surname',
+        id: 765432,
+        company: 'companyname',
+        role: 'role',
+        enddate: '20202020',
+      },
+      {
+        name: 'name',
+        surname: 'surname',
+        id: 765432,
+        company: 'companyname',
+        role: 'role',
+        enddate: '20202020',
+      },
+    ],
+    FirmenName: [
+      {
+        name: 'name',
+        surname: 'surname',
+        id: 765432,
+        company: 'companyname',
+        role: 'role',
+        enddate: '20202020',
+      },
+      {
+        name: 'name',
+        surname: 'surname',
+        id: 765432,
+        company: 'companyname',
+        role: 'role',
+        enddate: '20202020',
+      },
+    ],
+    CompanyName: [
+      {
+        name: 'name',
+        surname: 'surname',
+        id: 765432,
+        company: 'companyname',
+        role: 'role',
+        enddate: '20202020',
+      },
+      {
+        name: 'name',
+        surname: 'surname',
+        id: 765432,
+        company: 'companyname',
+        role: 'role',
+        enddate: '20202020',
+      },
+    ],
+  },
+  setopenCompany: () => {
+    return {
+      name: 'name',
+      surname: 'surname',
+      id: 765432,
+      company: 'companyname',
+      role: 'role',
+      enddate: '20202020',
+    }
+  },
+  setRenderNewWorker: () => {
+    return true
+  },
 }
